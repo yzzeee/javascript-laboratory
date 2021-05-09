8dd12f1584edbda3444a2f82fab9a4da2789680d
yzzeee <goooooodgood@gmail.com>
2021-05-09 16:28:19 +0900

    config: Add library
    - lodash, fxjs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Initial configuration of the laboratory');
+import lodash from 'lodash';
+import * as fxjs from 'fxjs';
+import * as S from 'fxjs/Strict';
+import * as C from 'fxjs/Concurrency';
+import * as L from 'fxjs/Lazy';
+
+window.lodash = lodash;
+window.fxjs = fxjs;
+window.S = S;
+window.C = C;
+window.L = L;

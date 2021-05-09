06c9b87992466fc08e22095be44be8831f9c75e6
yzzeee <goooooodgood@gmail.com>
2021-05-09 19:56:56 +0900

    config: Apply HMR
    - Using react-hot-loader
    - react-refresh, @pmmmwh/react-refresh-webpack-plugin is up to data, but
      there in an error in the previout version of react-dev-tools

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
+
 import JsonTable from '@components/JsonTable';
 
 const App = () => (
   <div>
+    afdfffffff
     <JsonTable />
   </div>
 );
-export default App;
+
+export default hot(module)(App);
